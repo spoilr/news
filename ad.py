a4f6a6d9ad34ef9b3a276929ec8f131edb7e6200
oana <oana160692@yahoo.com>
2016-01-17 23:47:35 +0000

    add more categories to ad.

--- ad.py
+++ ad.py
@@ -16,10 +16,56 @@ def ad_events():
 	main_titles = replace_new_lines(main_titles)
 	print_articles(main_titles)
 
+def ad_eu():
+	html = lxml.html.parse(a)
+	main_titles = html.xpath('//div[@id="main-content"]/div[@class="wrap clearfix"]/div[@class="column-large mixed-news"]/article[@class="international subcategory europa"]/h3[@class="defaultTitle"]/a/text()')
+	
+	main_titles = replace_new_lines(main_titles)
+	print_articles(main_titles)	
+
+def ad_politics():
+	html = lxml.html.parse(a)
+	main_titles = html.xpath('//div[@id="main-content"]/div[@class="wrap clearfix"]/div[@class="column-large mixed-news"]/article[@class="politica subcategory politica"]/h3[@class="defaultTitle"]/a/text()')
+	
+	main_titles = replace_new_lines(main_titles)
+	print_articles(main_titles)	
+
+def ad_economy():
+	html = lxml.html.parse(a)
+	main_titles = html.xpath('//div[@id="main-content"]/div[@class="wrap clearfix"]/div[@class="column-large mixed-news"]/article[@class="economie subcategory stiri-economice"]/h3[@class="defaultTitle"]/a/text()')
+	
+	main_titles = replace_new_lines(main_titles)
+	print_articles(main_titles)	
+
+def ad_school():
+	html = lxml.html.parse(a)
+	main_titles = html.xpath('//div[@id="main-content"]/div[@class="wrap clearfix"]/div[@class="column-large mixed-news"]/article[@class="educatie subcategory scoala"]/h3[@class="defaultTitle"]/a/text()')
+	
+	main_titles = replace_new_lines(main_titles)
+	print_articles(main_titles)
+
+def ad_society():
+	html = lxml.html.parse(a)
+	main_titles = html.xpath('//div[@id="main-content"]/div[@class="wrap clearfix"]/div[@class="column-large mixed-news"]/article[@class="societate subcategory societate"]/h3[@class="defaultTitle"]/a/text()')
+	
+	main_titles = replace_new_lines(main_titles)
+	print_articles(main_titles)	
+
 def main():
+	print "##### main"
 	ad_main_titles()
-	print "#####"
-	ad_events()	
+	print "##### events"
+	ad_events()
+	print "##### eu"
+	ad_eu()
+	print "##### politics"
+	ad_politics()
+	print "##### economy"
+	ad_economy()
+	print "##### school"
+	ad_school()
+	print "##### society"
+	ad_society()
 
 if __name__ == "__main__":
     main()
